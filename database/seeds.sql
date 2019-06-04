0f7b6eb2a01ac3c27e4755532bf267993a33558e
Lorenzo <lgdandrea4@gmail.com>
2019-06-03 20:37:01 -0500

    setup the initial boiler plate material needed to make the application work

---
+++ database/seeds.sql
@@ -0,0 +1,13 @@
+USE burgers_db;
+
+INSERT INTO burgers (burger_name)
+VALUES ('Cheesburger');
+
+INSERT INTO burgers (burger_name)
+VALUES ('Hamburger');
+
+INSERT INTO burgers (burger_name)
+VALUES ('Mushroom Swiss Burger');
+
+INSERT INTO burgers (burger_name)
+VALUES ('Bacon Cheesburger');
